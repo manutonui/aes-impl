dde69a0308c0653f3e4cd42c9fb19ac282f8f251
manutonui <tktonui@outlook.com>
2022-09-28 13:13:51 +0300

    Divide into block function divides input text to blocks of 16 bytes

--- index.py
+++ index.py
@@ -20,8 +20,23 @@ def encrypt(plainText, key):
     return cipherText
 
 
+# TASK 1: divide input text into blocks of 16 bytes
 def divideIntoBlocks(text):
-    return ""
+
+    if len(text) % 16 != 0:
+        text = padText(text) # Text padded
+
+    # Division of text to 16 bytes blocks
+    blocks = split_blocks(text)
+
+    byteBlocks = [] # Array of blocks of 16 bytes each
+    for block in blocks:
+        matrix = [] # one block of 16 bytes
+        for i in block:
+            matrix.append(ord(i)) # convert characters to byte values
+        byteBlocks.append(matrix)
+
+    return byteBlocks
 
 def getRoundKeys(key):
     return ""
@@ -41,3 +56,14 @@ def mixColumns(block):
 
 def reAssemble(blocks):
     return
+
+
+# Helper functions
+def padText(text):
+    for i in range(16):
+        if (len(text)+i) % 16 == 0:
+            return text+" "*i # add extra whitespace to make 16 bytes
+
+def split_blocks(message):
+        assert len(message) % 16 == 0
+        return [message[i:i+16] for i in range(0, len(message), 16)]
